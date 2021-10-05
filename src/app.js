c7bffca267171128d462d94ae6ec63bdeaef79d4
chandan008 <mishrachandan453@gmail.com>
2021-10-06 01:03:55 +0530

    message

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+import {BrowserRouter, Switch , Route } from 'react-router-dom';
+import Home from './pages/home';
+import AboutUs from "./pages/about";
+import Contact from "./pages/contact";
+import Product from "./pages/product";
+import LoginPage from "./login-page";
+
+
+class App extends Component{
+   
+    render(){
+        return(
+          <div> 
+            
+          </div>
+            
+        
+        )
+    }
+}
+export default App;
